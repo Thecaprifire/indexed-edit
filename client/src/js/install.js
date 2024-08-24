35a800adc4b7d5b3723519fb218edd2681ec847c
Ian Daluz <janiandaluz@yahoo.com>
2024-08-24 11:22:43 -0600

    added codes to complete task

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,46 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
+
+let deferredPrompt;
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+//Event handler for 'beforeinstallprompt' to show install button.
+window.addEventListener("beforeinstallprompt", (event) => {
+  // Prevent the browser's default install prompt
+  event.preventDefault();
+  // Store the event to trigger the prompt
+  window.deferredPrompt = event;
+  //show install button
+  butInstall.style.display = "block";
+});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+// Event handler for 'click' on the install button
+butInstall.addEventListener("click", async () => {
+  if (window.deferredPrompt) {
+    try {
+      // Wait for the user to respond
+      const choiceResult = await window.deferredPrompt.prompt();
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+      // Log user choice on installation
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the install prompt");
+      } else {
+        console.log("User dismissed the install prompt");
+      }
+
+      // Clear the deferredPrompt
+      window.deferredPrompt = null;
+
+      // Hide the install button.
+      butInstall.style.display = "none";
+    } catch (err) {
+      console.error("Installation error:", err);
+    }
+  } else {
+    console.log("No deferredPrompt available");
+  }
+});
+
+// Event handler for 'appinstalled' event after successful app installation
+window.addEventListener("appinstalled", (event) => {
+  console.log("App installed:", event);
+});
