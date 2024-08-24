68b478843ea606e471c8fa1cb1835bdbe6540290
Ian Daluz <janiandaluz@yahoo.com>
2024-08-24 11:26:16 -0600

    added codes to complete data

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,14 +1,19 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
-const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+// Used Module 19 activity 19 as a reference.
+// The StaleWhileRevalidate strategy has been added to serve content from cache and simultaneously load it from the source if required.
 
+const { offlineFallback, warmStrategyCache } = require("workbox-recipes");
+const { StaleWhileRevalidate, CacheFirst } = require("workbox-strategies");
+const { registerRoute } = require("workbox-routing");
+const { CacheableResponsePlugin } = require("workbox-cacheable-response");
+const { ExpirationPlugin } = require("workbox-expiration");
+const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
+
+// Define the precache rsources in WB_MANIFEST.
 precacheAndRoute(self.__WB_MANIFEST);
 
+// Define a CacheFirst for page cache with expiration
 const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
+  cacheName: "page-cache",
   plugins: [
     new CacheableResponsePlugin({
       statuses: [0, 200],
@@ -19,12 +24,41 @@ const pageCache = new CacheFirst({
   ],
 });
 
+// Warm cache strategy for specific URLs with the pageCache strategy
 warmStrategyCache({
-  urls: ['/index.html', '/'],
+  urls: ["/index.html", "/"],
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+// Check request mode for navigation
+registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  // Check if request destination is style, script, or worker
+  ({ request }) =>
+    request.destination === "style" ||
+    request.destination === "script" ||
+    request.destination === "worker",
+
+  new StaleWhileRevalidate({
+    cacheName: "asset-cache",
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
+
+// Register route for images using CacheFirst strategy
+registerRoute(
+  ({ request }) => request.destination === "image",
+  new CacheFirst({
+    cacheName: "image-cache",
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
