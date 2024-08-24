a9ae302622559677732328d1bc8672095c6f8f18
Ian Daluz <janiandaluz@yahoo.com>
2024-08-24 11:53:41 -0600

    edited codes

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,22 +16,28 @@ const initdb = async () =>
 
 // Function to add content to the database
 export const putDb = async (content) => {
-  const jateDb = await openDB("jate", 1);
-  const tx = jateDb.transaction("jate", "readwrite");
-  const store = tx.objectStore("jate");
-  const request = store.put({ id: 1, value: content });
+  console.log('Put to the Database');
+ 
+  const contactDb = await openDB('jate', 1);
+  const tx = contactDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({id: 1,value: content});
   const result = await request;
-  console.log("Data saved to the database", result, content);
+  console.log('Saved to Database', result);
 };
+
 
 // Function to get or retrieve content from the database
 export const getDb = async () => {
-  const jateDb = await openDB("jate", 1);
-  const tx = jateDb.transaction("jate", "readonly");
-  const store = tx.objectStore("jate");
-  const request = store.getAll();
+  console.log('Get to the Database')
+
+  const contactDb = await openDB('jate', 1);
+  const tx = contactDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
   const result = await request;
-  return result.value;
+  console.log('result.value', result);
+  return result?.value;
 };
 
 initdb();
